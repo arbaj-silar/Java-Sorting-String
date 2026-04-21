41933d43fcc9da373584aa1ec3acfafb8f5f6dcf
Arbaj <silararbaj@gmail.com>
2026-04-21 17:03:04 +0530

    Removing Occurrences

--- I_RemoveOccurrences.java
+++ I_RemoveOccurrences.java
@@ -1,3 +1,18 @@
 public class I_RemoveOccurrences {
-    
+    public static String removeOccurrence(String s, String part) {
+        StringBuilder str = new StringBuilder(s);
+        int idx;
+        while((idx = str.indexOf(part)) != -1) {
+            str.delete(idx, idx+part.length());
+        }
+
+        return str.toString();
+    } 
+
+    public static void main(String[] args) {
+        String s = "daabcaabcbc";
+        String part = "abc";
+
+        System.out.println("Left String: "+removeOccurrence(s, part));
+    }
 }
